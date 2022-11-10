9c472867f458a18c5b0d7fe0caa7d45a594411ba
Alvin <hello@alvin.codes>
2022-11-10 20:01:20 +0000

    lint

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 import express from 'express'
-const app = express();
+const app = express()
 import dotenv from 'dotenv'
 import bodyParser from 'body-parser'
 dotenv.config()
 
-import CMSRoutes from './routes/cmsroutes.js';
+import CMSRoutes from './routes/cmsroutes.js'
 
 // settings
-app.set("port", process.env.PORT || 8000);
-app.use(bodyParser.json());
+app.set('port', process.env.PORT || 8000)
+app.use(bodyParser.json())
 
 // routes
-app.use('/',CMSRoutes)
+app.use('/', CMSRoutes)
 
 // listening the Server
-app.listen(app.get("port"), () => {
-  console.log("Server on port", app.get("port"));
-});
+app.listen(app.get('port'), () => {
+  console.log('Server on port', app.get('port'))
+})
